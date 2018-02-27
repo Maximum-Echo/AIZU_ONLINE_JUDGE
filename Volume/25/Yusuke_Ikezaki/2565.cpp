fe435b45e33bbb9314e31e1e22e76e68ba127abf
Yusuke Ikezaki <IappleY08@gmail.com>
2018-02-28 02:04:57 +0900

    Add 25XX files

---
+++ Volume/25/Yusuke_Ikezaki/2565.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int stoi(string s){
+  int num=0;
+  for(int i=s.size()-1,digit=1;i>=0;i--,digit*=10){
+    if(s[i]=='-') num*=-1; 
+    else num+=(s[i]-'0')*digit;
+  }
+  return num;
+}
+
+int main(){
+  int n;
+  while(cin>>n,n){
+    string sig[1001];
+    sig[0]="1000000001";
+    for(int i=1;i<=n;i++)
+      cin>>sig[i];
+    if(n%2==0) sig[n+1]="-1000000001";
+    else sig[n+1]="1000000001";
+    int Min=-100000001,Max=1000000001;
+    for(int i=1;i<=n;i++){
+      if(Min>Max){
+	cout<<"none"<<endl; break;
+      }
+      if(sig[i]=="x"){
+	if(sig[i+1]=="x"){
+	  cout<<"none"<<endl; break;
+	}
+	if(i%2==1){
+	  if(sig[i-1]);
+      } else{
+      }
+    }
+  }
+}
